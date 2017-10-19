14524b49e70cf765c45e2ba94b53f9649143b07d
Gaius Mathew <gaiusmathew1996@gmail.com>
2017-10-19 16:01:55 +0530

    edited database

--- Db_conection.php
+++ Db_conection.php
@@ -1,10 +1,12 @@
 <?php  
 /** 
- * Created by PhpStorm. 
- * User: Ehtesham Mehmood 
- * Date: 11/21/2014 
- * Time: 1:13 AM 
+ * Created by Gaius Mathew 
+ * www.github.com/gaiusmathew 
+ * For more visit : github.com/bug-coder
+ * change hostname, username, password with your own values
  */  
-$dbcon=mysqli_connect("localhost","root","1996");  
-mysqli_select_db($dbcon,"login");  
+
+
+$dbcon=mysqli_connect("your-host-name","user-name","password");  
+mysqli_select_db($dbcon,"dbname");  
 ?>  
